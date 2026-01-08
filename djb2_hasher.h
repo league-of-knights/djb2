7ff6029552d10b66b2e0efce257f5fb79457d12d
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2026-01-08 09:02:55 +0300

    Fix C++23 compatibility: literal operator decl & clang calculate() accessibility

--- djb2_hasher.h
+++ djb2_hasher.h
@@ -24,14 +24,26 @@ namespace hashing
 
     struct djb2
     {
+    private:
+        template <character_range Range>
+        static constexpr hash_type calculate( const Range &range, hash_type seed = 5381 ) noexcept
+        {
+            hash_type h = seed;
+            for ( auto &&element: range )
+            {
+                h = ( ( h << 5 ) + h ) + static_cast<hash_type>(element);
+            }
+            return h;
+        }
+
     public:
         consteval djb2( std::string_view djb ) noexcept : _hash( calculate( djb ) )
         {
-        };
+        }
 
         consteval djb2( std::wstring_view wdjb ) noexcept : _hash( calculate( wdjb ) )
         {
-        };
+        }
 
         constexpr hash_type operator()( ) const noexcept
         {
@@ -61,25 +73,14 @@ namespace hashing
 
     private:
         hash_type _hash;
-
-        template <character_range Range>
-        static constexpr hash_type calculate( const Range &range, hash_type seed = 5381 ) noexcept
-        {
-            hash_type h = seed;
-            for ( const auto &element: range )
-            {
-                h = ( ( h << 5 ) + h ) + element;
-            }
-            return h;
-        }
     };
 
-    consteval hash_type operator"" _djb2( const char *str, size_t len ) noexcept
+    consteval hash_type operator""_djb2( const char *str, size_t len ) noexcept
     {
         return djb2( std::string_view{ str, len } );
     }
 
-    consteval hash_type operator"" _djb2w( const wchar_t *str, size_t len ) noexcept
+    consteval hash_type operator""_djb2w( const wchar_t *str, size_t len ) noexcept
     {
         return djb2( std::wstring_view{ str, len } );
     }
